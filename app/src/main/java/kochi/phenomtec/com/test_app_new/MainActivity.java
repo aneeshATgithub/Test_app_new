1041882947d71daa08a01e45c234424299810b0a
Aneesh <aneesh@phenomtec.com>
2017-10-04 11:35:37 +0530

    my frist commit 11:35

---
+++ app/src/main/java/kochi/phenomtec/com/test_app_new/MainActivity.java
@@ -0,0 +1,13 @@
+package kochi.phenomtec.com.test_app_new;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
